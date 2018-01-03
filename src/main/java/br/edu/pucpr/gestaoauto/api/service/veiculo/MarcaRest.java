958f8092ddec086ecfd6437b96a201f4f2ce96a2
Rafael Maciel Vanat <rafaelvant@gmail.com>
2018-01-03 15:21:35 -0200

    Inclusão demais operações crud para marca

--- src/main/java/br/edu/pucpr/gestaoauto/api/service/veiculo/MarcaRest.java
+++ src/main/java/br/edu/pucpr/gestaoauto/api/service/veiculo/MarcaRest.java
@@ -7,6 +7,7 @@ import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+import br.edu.pucpr.gestaoauto.api.Message;
 import br.edu.pucpr.gestaoauto.api.service.AbstractRest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -67,4 +68,40 @@ public class MarcaRest extends AbstractRest {
 			return this.serverError(e);
 		}
 	}
+
+    @PUT
+    @Path("/{codigo}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response update (@PathParam("codigo") Integer codigo, String nome){
+        log.info("MarcaRest -> update -> codigo " + codigo + " > nome " + nome);
+        try {
+            MarcaDTO dto = new MarcaDTO();
+            dto.setCodigo(codigo);
+            dto.setNome(nome);
+            return Response.ok().entity(
+                    manager.convertEntityToDTO(
+                            manager.update(
+                                    manager.convertDTOToEntity(
+                                            dto))))
+                    .build();
+        } catch (Exception e) {
+            log.error(e.toString());
+            return this.serverError(e);
+        }
+    }
+
+    @DELETE
+    @Path("/{codigo}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response update (@PathParam("codigo") Integer codigo){
+        log.info("MarcaRest -> delete");
+        try {
+            manager.delete(codigo);
+            return Response.ok().entity(new Message("Sucesso")).build();
+        } catch (Exception e) {
+            log.error(e.toString());
+            return this.serverError(e);
+        }
+    }
 }
