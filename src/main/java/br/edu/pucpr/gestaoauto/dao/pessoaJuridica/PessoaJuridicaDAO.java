0ccf8d56b624073a4f350244c0a6a16401eb0ffd
Rafael Maciel Vanat <rafaelvant@gmail.com>
2018-02-05 02:26:48 -0200

    Adicionadas funções para pessoa juridica para viabilizar cadastro de manutenção

--- src/main/java/br/edu/pucpr/gestaoauto/dao/pessoaJuridica/PessoaJuridicaDAO.java
+++ src/main/java/br/edu/pucpr/gestaoauto/dao/pessoaJuridica/PessoaJuridicaDAO.java
@@ -1,10 +1,35 @@
 package br.edu.pucpr.gestaoauto.dao.pessoaJuridica;
 
-import javax.ejb.Stateless;
-
 import br.edu.pucpr.gestaoauto.dao.DAO;
 import br.edu.pucpr.gestaoauto.model.pessoaJuridica.PessoaJuridica;
+import br.edu.pucpr.gestaoauto.model.pessoaJuridica.Reparador;
+import br.edu.pucpr.gestaoauto.model.pessoaJuridica.Seguradora;
+
+import javax.ejb.Stateless;
+import javax.ejb.TransactionAttribute;
+import javax.ejb.TransactionAttributeType;
+import javax.persistence.TypedQuery;
+import java.util.List;
 
 @Stateless
 public class PessoaJuridicaDAO extends DAO<Integer, PessoaJuridica> {
+    @TransactionAttribute(TransactionAttributeType.SUPPORTS)
+    public List<Reparador> getReparadorasByNome(String nome) {
+        TypedQuery<Reparador> query = super.entityManager.createQuery(
+                "select p from Reparador p where p.nomeFantasia = :nome order by p.nomeFantasia desc"
+                , Reparador.class);
+        query.setParameter("nome", nome);
+        query.setMaxResults(10);
+        return query.getResultList();
+    }
+
+    @TransactionAttribute(TransactionAttributeType.SUPPORTS)
+    public List<Seguradora> getSeguradorasByNome(String nome) {
+        TypedQuery<Seguradora> query = super.entityManager.createQuery(
+                "select p from Seguradora p where p.nomeFantasia = :nome order by p.nomeFantasia desc"
+                , Seguradora.class);
+        query.setParameter("nome", nome);
+        query.setMaxResults(10);
+        return query.getResultList();
+    }
 }
--- src/main/java/br/edu/pucpr/gestaoauto/manager/PessoaJuridicaManager.java
+++ src/main/java/br/edu/pucpr/gestaoauto/manager/PessoaJuridicaManager.java
@@ -1,8 +1,12 @@
 package br.edu.pucpr.gestaoauto.manager;
 
+import br.edu.pucpr.gestaoauto.api.dto.manutencao.ManutencaoDTO;
 import br.edu.pucpr.gestaoauto.api.dto.pessoaJuridica.PessoaJuridicaDTO;
 import br.edu.pucpr.gestaoauto.dao.pessoaJuridica.PessoaJuridicaDAO;
 import br.edu.pucpr.gestaoauto.model.pessoaJuridica.PessoaJuridica;
+import br.edu.pucpr.gestaoauto.model.pessoaJuridica.Reparador;
+import br.edu.pucpr.gestaoauto.model.pessoaJuridica.Seguradora;
+import br.edu.pucpr.gestaoauto.util.ObjetoNaoEncontradoException;
 
 import javax.ejb.LocalBean;
 import javax.ejb.Stateless;
@@ -56,4 +60,45 @@ public class PessoaJuridicaManager implements Manager<Integer, PessoaJuridica> {
 		pessoaJuridicaDTO.setRazaosocial(pessoaJuridica.getRazaoSocial());
 		return pessoaJuridicaDTO;
 	}
+
+	public Reparador carregaReparador(ManutencaoDTO dto) throws ObjetoNaoEncontradoException {
+		Reparador reparador;
+		if(dto.getCodigoReparador() == null && dto.getNomeReparador() == null){
+		    throw new ObjetoNaoEncontradoException("error.pessoa.juridica.naoEncontrado",
+                    new Object[] { dto.getCodigoReparador(), dto.getNomeReparador() });
+        }
+        else if(dto.getCodigoReparador() == null && dto.getNomeReparador() != null) {
+            List<Reparador> reparadores = pessoaJuridicaDAO.getReparadorasByNome(dto.getNomeReparador());
+            if(reparadores.isEmpty()) {
+                reparador = new Reparador();
+                reparador.setNomeFantasia(dto.getNomeReparador());
+                this.save(reparador);
+            } else reparador = reparadores.get(0);
+		}
+		else {
+			reparador = (Reparador) this.getById(dto.getCodigoReparador());
+		}
+		return reparador;
+	}
+
+    public Seguradora carregaSeguradora(ManutencaoDTO dto) throws ObjetoNaoEncontradoException {
+        Seguradora seguradora;
+        if(dto.getCodigoSeguradora() == null && dto.getNomeSeguradora() == null){
+            throw new ObjetoNaoEncontradoException("error.pessoa.juridica.naoEncontrado",
+                    new Object[] { dto.getCodigoSeguradora(), dto.getNomeSeguradora() });
+        }
+        else if(dto.getCodigoSeguradora() == null && dto.getNomeSeguradora() != null) {
+            List<Seguradora> seguradoras = pessoaJuridicaDAO.getSeguradorasByNome(dto.getNomeSeguradora());
+            if(seguradoras.isEmpty()) {
+                seguradora = new Seguradora();
+                seguradora.setNomeFantasia(dto.getNomeSeguradora());
+                this.save(seguradora);
+            }
+            else seguradora = seguradoras.get(0);
+        }
+        else {
+            seguradora = (Seguradora) this.getById(dto.getCodigoSeguradora());
+        }
+        return seguradora;
+    }
 }
